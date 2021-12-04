fec98aa9a9086bc0c643d74d5a8e0caeeb3d0118
iamkhalil <medkhalilelb@gmail.com>
2021-12-04 16:44:25 +0100

    Add Tail recursive solution

--- p04.ml
+++ p04.ml
@@ -1,4 +1,20 @@
+(* Non-tail recursive solution *)
 let rec length (my_list: 'a list): int =
     match my_list with
     | [] -> 0
     | _ :: rest -> 1 + length rest
+
+
+(* Tail recursive solution *)
+let rec length_tail (my_list: 'a list) (result: int): int =
+    match my_list with
+    | [] -> result
+    | _ :: rest -> length_tail rest (result + 1)
+
+(* Tail recursive solution, but in an elegant way *)
+let length_tail_wrapper (my_list: 'a list): int =
+    let rec length_tail_rec (my_list: 'a list) (result: int): int =
+        match my_list with
+        | [] -> result
+        | _ :: rest -> length_tail_rec rest (result + 1)
+    in length_tail_rec my_list 0
